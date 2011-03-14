feaf127c0d99dc80b750ae36dbe3ea27f9694e96
Donovan Bray <donnoman@donovanbray.com>
2011-03-13 18:02:26 -0700

    initial commit

---
+++ configuration.php
@@ -0,0 +1,24 @@
+<?php
+/*************************
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2005 Coppermine Dev Team
+  v1.1 originaly written by Gregory DEMAR
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  ********************************************
+  Coppermine version: 1.4.1
+  $Source: /cvsroot/cpg-contrib/visiblehookpoints/configuration.php,v $
+  $Revision: 1.3 $
+  $Author: donnoman $
+  $Date: 2006/11/13 02:47:55 $
+**********************************************/
+
+$name='Visible HookPoints';
+$description='Plugin Developer tool to locate plugin entry points and array information';
+$author='Donnoman@donovanbray.com from <a href="http://cpg-contrib.org" target="_blank">cpg-contrib.org</a>';
+$version='2.1';
+?>
